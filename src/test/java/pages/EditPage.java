7fe627cc50018d039fc17a3510086e093f4a57f0
Julian Riedinger <julianriedinger27@gmail.com>
2023-11-13 19:39:59 -0300

    added edit page

---
+++ src/test/java/pages/EditPage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class EditPage extends BasePage {
+
+  @FindBy(id = "firstHeading")
+  private WebElement editTitle;
+
+  @FindBy(className = "wikiEditor-ui")
+  private WebElement editorContainer;
+
+  public EditPage(WebDriver driver) {
+    super(driver);
+  }
+
+  public boolean isEditTitleCorrect(String text) {
+    return isTitleCorrect(text, editTitle);
+  }
+
+  public boolean isEditorDisplayed() {
+    return isElementDisplayed(editorContainer);
+  }
+  
+
+}
